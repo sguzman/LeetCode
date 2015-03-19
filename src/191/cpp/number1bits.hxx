49810a2451c591e572501eaadf782bc8e9881d71
Salvador Guzman <guzmansalv@gmail.com>
2015-03-18 23:10:29 -0700

    solution for 191

---
+++ src/191/cpp/number1bits.hxx
@@ -0,0 +1,12 @@
+class Solution {
+public:
+	int hammingWeight(uint32_t n) {
+		uint32_t count{};
+		for (uint32_t i{}; i < 32; i++) {
+			count += n & 1;
+			n >>= 1;
+		}
+
+		return count;
+	}
+};
