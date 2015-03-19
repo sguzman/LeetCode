580b6ebf1ee3396e403eca093cff7c59fa3da2fb
Salvador Guzman <sguzman@liaison.com>
2015-03-19 10:21:14 -0700

    add executable for number1bits to test

---
+++ src/191/cpp/main.cxx
@@ -0,0 +1,18 @@
+//
+// Created by SGuzman on 3/19/2015.
+//
+#include <iostream>
+
+#include "number1bits.hxx"
+
+using namespace std;
+
+int main(void) {
+	Solution sol;
+
+	for (int i = 0; i < 10; ++i) {
+		cout << sol.hammingWeight(i) << endl;
+	}
+
+	return 0;
+}
