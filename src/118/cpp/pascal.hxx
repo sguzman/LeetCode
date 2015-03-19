7548e2c4c5088e8bef5394187a2cf9e509be8b7a
Salvador Guzman <guzmansalv@gmail.com>
2015-03-18 21:56:33 -0700

    pascal's triangle generator

---
+++ src/118/cpp/pascal.hxx
@@ -0,0 +1,22 @@
+class Solution {
+public:
+	vector<vector<int>> generate(int numRows) {
+		vector<vector<int>> final;
+
+		if (numRows >= 1) {
+			final.push_back(vector<int>{1});
+		}
+
+		for (int i = 1; i < numRows; ++i) {
+
+			final.push_back(vector<int>{1});
+			for (int j = 1; j < i; ++j) {
+				final[i].push_back(final[i - 1][j - 1] + final[i - 1][j]);
+			}
+
+			final[i].push_back(1);
+		}
+
+		return final;
+	}
+};
