6e9eb86ad098eccb2f42ebb5dd85032ba8c97ad2
Salvador Guzman <sguzman@liaison.com>
2015-03-19 09:32:29 -0700

    test executable for pascal

---
+++ src/118/cpp/main.cxx
@@ -0,0 +1,18 @@
+//
+// Created by SGuzman on 3/19/2015.
+//
+#include <iostream>
+
+#include "pascal.hxx"
+
+using namespace std;
+
+int main(void) {
+	Solution sol;
+
+	for (int i = 0; i < 10; ++i) {
+		cout << sol.generate(i) << endl;
+	}
+
+	return 0;
+}
