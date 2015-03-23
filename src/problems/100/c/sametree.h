a778657de3038d258db1673015bac716a2284cd9
Salvador Guzman <guzmansalv@gmail.com>
2015-03-22 18:11:51 -0700

    solution in c

---
+++ src/problems/100/c/sametree.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "TreeNode.h"
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSameTree(struct TreeNode *p, struct TreeNode *q) {
+    if(!p || !q) {
+        return p == q;
+    }
+
+    return p->val==q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+}
