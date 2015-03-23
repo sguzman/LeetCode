5b123efdcc69172d255fbdb9d2a9eba3ead7c2a8
Salvador Guzman <guzmansalv@gmail.com>
2015-03-22 19:26:31 -0700

    added solution to maxdepth in c

---
+++ src/problems/104/c/maxdepth.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stddef.h>
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+  * };
+ */
+int max(int a, int b) {
+    if (a > b) {
+        return a;
+    }
+
+    return b;
+}
+
+int maxDepth(struct TreeNode *root) {
+    if(root == NULL) {
+        return 0;
+    }
+
+    return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+}
