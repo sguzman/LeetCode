212eb75bd762a3169ba9b015f2dc72c84cfbd707
Salvador Guzman <guzmansalv@gmail.com>
2015-03-23 22:05:17 -0700

    cleaner solution

--- src/problems/28/cpp/naive/strstr.hxx
+++ src/problems/28/cpp/naive/strstr.hxx
@@ -3,26 +3,12 @@
 class Solution {
 public:
     int strStr(char* haystack, char* needle) {
-        if (*haystack == '\0' && *needle == '\0') {
-            return 0;
-        }
-
-        int idx;
-
         for (int i = 0; haystack[i] != '\0'; ++i) {
-            if (haystack[i] == needle[0]) {
-
-                for (idx = 0; needle[idx] != '\0'; ++idx) {
-                    if (haystack[i + idx] != needle[idx]) {
-                        idx = -1;
-                        break;
+                for (int j = 0; haystack[i + j] != '\0' && haystack[i + j] == needle[j]; ++j) {
+                    if (needle[j + 1] == '\0') {
+                        return i;
                     }
                 }
-
-                if (idx != -1) {
-                    return i;
-                }
-            }
         }
 
         return -1;
