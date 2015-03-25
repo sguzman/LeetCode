8e993f6a003e4d6e739dee58e4cf6bc053e956b7
Salvador Guzman <guzmansalv@gmail.com>
2015-03-24 21:48:33 -0700

    current state of KMP solution attempting to optimize outer loop first pretty difficult...

--- src/problems/28/cpp/kmp/strstrKMP.hxx
+++ src/problems/28/cpp/kmp/strstrKMP.hxx
@@ -3,14 +3,27 @@
 class Solution {
 public:
     int strStr(char* haystack, char* needle) {
-        int m, n;
-        for (m = 0, n = 0; haystack[m] != '\0'; ++m) {
+        int m, n, j;
+        for (m = n = 0; haystack[m] != '\0'; m += n) {
             if (haystack[m] == needle[0]) {
-                for (n = 0; haystack[m + n] != '\0' && needle[n] != '\0' && haystack[m + n] == needle[n]; ++n);
+                for ((j = n), n = 0; haystack[m + j] != '\0' && needle[j] != '\0' && haystack[m + j] == needle[j]; ++j) {
+                    if (j > n) {
+                        if (haystack[m + j] == needle[n]) {
+                            ++n;
+                        } else {
+                            n = 0;
+                        }
+                    }
+                }
 
-                if (needle[n] == '\0') {
+                if (needle[j] == '\0') {
                     return m;
                 }
+
+                if (n == 0) {
+                    n = j;
+                }
+
             }
         }
 
