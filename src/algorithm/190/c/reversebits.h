499a0b41b896f127515ebba9cc58a51b04609bb0
Salvador Guzman <guzmansalv@gmail.com>
2015-04-12 13:33:01 -0700

    solution for reverse bits in c

---
+++ src/algorithm/190/c/reversebits.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdint.h>
+
+uint32_t reverseBits(uint32_t n) {
+  uint32_t final = 0;
+  for (uint32_t i = 0; n != 0; i++, n >>= 1) {
+    final |= (n & 1) << 31 - i;
+  }
+
+  return final;
+}
