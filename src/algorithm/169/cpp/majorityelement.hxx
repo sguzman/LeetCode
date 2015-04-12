98d64c9bc53182f40e3ec9985e4587d34a136b06
Salvador Guzman <guzmansalv@gmail.com>
2015-04-12 14:00:01 -0700

    solution in c++

---
+++ src/algorithm/169/cpp/majorityelement.hxx
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+
+class Solution{
+public:
+  int majorityElement(std::vector<int> &sum) {
+    int major{sum[0]}, freq{};
+
+    for (int i = 0; i < sum.size(); ++i) {
+      if (sum[i] == major) {
+        ++freq;
+      } else {
+        if (--freq == 0) {
+          freq = 1;
+          major = sum[i];
+        }
+      }
+    }
+
+    return major;
+  }
+};
