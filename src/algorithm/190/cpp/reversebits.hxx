06f785667bd29afa525a2f4e97736a0bb14e9a6e
Salvador Guzman <guzmansalv@gmail.com>
2015-04-12 13:24:18 -0700

    solution for reverse bits

---
+++ src/algorithm/190/cpp/reversebits.hxx
@@ -0,0 +1,13 @@
+#pragma once
+
+class Solution {
+public:
+  uint32_t reverseBits(uint32_t n) {
+    uint32_t final{};
+    for (uint32_t i{}; n != 0; i++, n >>= 1) {
+      final |= (n & 1) << 31 - i;
+    }
+
+    return final;
+  }
+};
