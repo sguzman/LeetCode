c4d21ab84be3689ce4a9b88782d4be17ff53de1a
Salvador Guzman <guzmansalv@gmail.com>
2015-04-12 12:45:58 -0700

    added driver for c++ solution

---
+++ src/algorithm/171/cpp/main.cxx
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <array>
+#include "excelnumber.hxx"
+
+using namespace std;
+
+int main(void) {
+  Solution sol;
+  std::array<std::string,5> list{"A", "B", "C", "D", "AA"};
+
+  for (auto& i : list) {
+    cout << sol.titleToNumber(i) << endl;
+  }
+
+  return EXIT_SUCCESS;
+}
