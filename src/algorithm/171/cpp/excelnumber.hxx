a0739c34ab19e11ee01dcd2bc4bfbbc289cab546
Salvador Guzman <guzmansalv@gmail.com>
2015-04-12 12:40:16 -0700

    added solution for 171 in c++

---
+++ src/algorithm/171/cpp/excelnumber.hxx
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+class Solution {
+public:
+  int titleToNumber(std::string s) {
+    int sum{};
+
+    for (int down{s.size() - 1}, up{}, coef26{1}; up < s.size(); (sum += (static_cast<int>(s[down]) - 64) * coef26), ++up, --down, coef26 *= 26);
+
+    return sum;
+  }
+};
