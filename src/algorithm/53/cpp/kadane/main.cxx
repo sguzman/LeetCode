8c591cab482936a58a9631ac73daed54044bd611
Salvador Guzman <guzmansalv@gmail.com>
2015-04-15 01:28:20 -0700

    added third failed answer as test case

--- src/algorithm/53/cpp/kadane/main.cxx
+++ src/algorithm/53/cpp/kadane/main.cxx
@@ -10,9 +10,11 @@ int main(void) {
   Solution sol;
   std::vector<int> array1{-2,1,-3,4,-1,2,1,-5,4};
   std::vector<int> array2{-1};
+  std::vector<int> array3{1};
 
   Test::assertEquals<int>(6, sol.maxSubArray(array1));
   Test::assertEquals<int>(-1, sol.maxSubArray(array2));
+  Test::assertEquals<int>(1, sol.maxSubArray(array3));
 
   return EXIT_SUCCESS;
 }
