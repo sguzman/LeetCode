7b37b79fd1f6ea3b4798f92f423fb7f6acc09d4a
Salvador Guzman <guzmansalv@gmail.com>
2015-04-14 23:54:32 -0700

    solution to max subarray sum using kadanes method

--- src/algorithm/53/cpp/kadane/maxsum.hxx
+++ src/algorithm/53/cpp/kadane/maxsum.hxx
@@ -3,6 +3,18 @@
 class Solution {
 public:
   int maxSubArray(int A[], int n) {
+    int maxSum{}, sumSoFar{ };
 
+    for (int i = 0; i < n; ++i) {
+      sumSoFar += A[i];
+
+      if (sumSoFar < 0) {
+        sumSoFar = 0;
+      } else if (maxSum < sumSoFar) {
+        maxSum = sumSoFar;
+      }
+    }
+
+    return maxSum;
   }
 };
