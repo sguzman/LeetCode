7b4bdd3312298c0773803ddc28b8ddcc4cb40eec
Salvador Guzman <guzmansalv@gmail.com>
2015-04-15 00:48:45 -0700

    adding a minimal test framework using it for the first time in problem 53 driver added to cmake build for 53

--- src/algorithm/53/cpp/kadane/main.cxx
+++ src/algorithm/53/cpp/kadane/main.cxx
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include "maxsum.hxx"
+#include "../../../../include/cpp/test.hxx"
 
 using namespace std;
 
@@ -8,7 +9,10 @@ int main(void) {
   Solution sol;
   int array[9] {-2,1,-3,4,-1,2,1,-5,4};
 
-  cout << sol.maxSubArray(array, 9) << endl;
+  int actual = sol.maxSubArray(array, 9) + 1;
+  int expected = 6;
+
+  Test::assertEquals(expected, actual);
 
   return EXIT_SUCCESS;
 }
---
+++ src/include/cpp/test.hxx
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+
+struct Test final {
+  template <typename... A>
+  explicit Test(A...) = delete;
+
+  template <typename A>
+  void operator=(A) = delete;
+
+  template <typename A>
+  operator A(void) = delete;
+
+  template <typename A>
+  static void assertEquals(const char* const msg, A expected, A actual) {
+    if (expected == actual) {
+      cout << "SUCCESS:" << endl
+           << "\texpected(" << expected << ") ";
+    } else {
+      cout << "FAILURE:" << endl
+           << "\t" << msg << ": " << "expected(" << expected << ") !";
+    }
+
+    cout << "= actual(" << actual << ")." << endl;
+  }
+
+  template <typename A>
+  static void assertEquals(std::string msg, A expected, A actual) {
+    Test::assertEquals<A>(msg.c_str(), expected, actual);
+  }
+
+  template <typename A>
+  static void assertEquals(A expected, A actual) {
+    Test::assertEquals<A>("Actual does not match expected", expected, actual);
+  }
+};
