6b9c6a9059a60e9362d2dfd61888aa263ecc1842
Salvador Guzman <guzmansalv@gmail.com>
2015-04-23 02:53:33 -0700

    added more test cases

--- src/algorithm/28/cpp/horspool/main.cxx
+++ src/algorithm/28/cpp/horspool/main.cxx
@@ -24,12 +24,20 @@ int main(void) {
   std::string haystack5{"mississippi"};
   std::string needle5{"issipi"};
 
-  std::vector<int> haystack{42, 10, 0, -1};
+  std::string haystack6{"mississippi"};
+  std::string needle6{"missi"};
+
+  std::string haystack7{"mississippi"};
+  std::string needle7{"issippi"};
+
+  std::vector<int> haystack{42, 10, 0, -1, -1, 0, 4};
   std::vector<int> needle{sol.strStr(haystack1, needle1),
                             sol.strStr(haystack2, needle2),
                             sol.strStr(haystack3, needle3),
                             sol.strStr(haystack4, needle4),
-                            sol.strStr(haystack5, needle5)};
+                            sol.strStr(haystack5, needle5),
+                            sol.strStr(haystack6, needle6),
+                            sol.strStr(haystack7, needle7)};
 
   Test::assertEquals<int, std::vector, std::vector>(haystack, needle);
 
