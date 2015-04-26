cc475e8f775cb6fb8e673448bafff21fcb58670b
Salvador Guzman <guzmansalv@gmail.com>
2015-04-26 12:05:06 -0700

    solution for 96

--- src/problems/algorithm/96/cpp/uniquebst.hxx
+++ src/problems/algorithm/96/cpp/uniquebst.hxx
@@ -2,14 +2,45 @@
 
 #include <unordered_map>
 
+using ull = unsigned long long;
+
 class Solution {
 public:
   int numTrees(int n) {
-    unsigned long long ret = static_cast<unsigned long long>(n);
+    ull ret = static_cast<unsigned long long>(n);
+
+    ret = f(ret);
 
     return static_cast<int>(ret);
   }
 
 private:
-  std::unordered_map<unsigned long long, unsigned long long> fHashMap;
+  inline ull f(ull n) noexcept {
+    if (exists(n)) {
+      return fHashMap[n];
+    } else {
+      if (n <= 1ull) {
+        return fHashMap[n] = 1ull;
+      } else {
+        auto sum = 0ull;
+
+        for (auto i = 0ull; i < n; ++i) {
+          sum += f(i) + f(n - i);
+        }
+
+        return fHashMap[n] = sum;
+      }
+    }
+  }
+
+  inline bool exists(ull key) {
+    try {
+      fHashMap.at(key);
+      return true;
+    } catch(...) {
+      return false;
+    }
+  }
+
+  std::unordered_map<ull,ull> fHashMap;
 };
