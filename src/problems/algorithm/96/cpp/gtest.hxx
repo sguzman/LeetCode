28567f9829bbb15c6924e82d740af56192ac1fbd
Salvador Guzman <guzmansalv@gmail.com>
2015-04-30 10:36:51 -0700

    gtest test file for unique bst

---
+++ src/problems/algorithm/96/cpp/gtest.hxx
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <gtest/gtest.h>
+#include "uniquebst.hxx"
+
+namespace
+{
+  class UniqueBSTTest : public testing::Test
+  {
+  protected:
+    UniqueBSTTest(void) {}
+    virtual ~UniqueBSTTest(void) {}
+    virtual void SetUp(void) {}
+    virtual void TearDown(void) {}
+  };
+
+  TEST(UniqueBSTTest, Unique1)
+  {
+    auto actual = Solution{}.numTrees(1);
+    decltype(actual) expected = 1;
+
+    EXPECT_EQ(expected, actual);
+  }
+
+  TEST(UniqueBSTTest, Unique2)
+  {
+    auto actual = Solution{}.numTrees(2);
+    decltype(actual) expected = 2;
+
+    EXPECT_EQ(expected, actual);
+  }
+
+  TEST(UniqueBSTTest, Unique3)
+  {
+    auto actual = Solution{}.numTrees(3);
+    decltype(actual) expected = 5;
+
+    EXPECT_EQ(expected, actual);
+  }
+
+  TEST(UniqueBSTTest, Unique4)
+  {
+    auto actual = Solution{}.numTrees(4);
+    decltype(actual) expected = 14;
+
+    EXPECT_EQ(expected, actual);
+  }
+
+  TEST(UniqueBSTTest, Unique5)
+  {
+    auto actual = Solution{}.numTrees(5);
+    decltype(actual) expected = 42;
+
+    EXPECT_EQ(expected, actual);
+  }
+
+  TEST(UniqueBSTTest, Unique6)
+  {
+    auto actual = Solution{}.numTrees(6);
+    decltype(actual) expected = 132;
+
+    EXPECT_EQ(expected, actual);
+  }
+
+  TEST(UniqueBSTTest, Unique7)
+  {
+    auto actual = Solution{}.numTrees(7);
+    decltype(actual) expected = 429;
+
+    EXPECT_EQ(expected, actual);
+  }
+}
