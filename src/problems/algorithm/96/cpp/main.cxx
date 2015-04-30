97c509e7ab5ee7f001273350d440b6444c845608
Salvador Guzman <guzmansalv@gmail.com>
2015-04-30 05:57:19 -0700

    simple main for gtest tests

--- src/problems/algorithm/96/cpp/main.cxx
+++ src/problems/algorithm/96/cpp/main.cxx
@@ -1,20 +1,10 @@
 #include <iostream>
-#include <cstdlib>
-#include <vector>
-#include "uniquebst.hxx"
-#include "../../../../include/cpp/test.hxx"
+#include <gtest/gtest.h>
+#include "gtest.hxx"
 
 using namespace std;
 
-int main(void) {
-  Solution sol;
-  auto process = [&sol] (int n) { return sol.numTrees(n);};
-
-  vector<int> args{1, 2, 3, 4, 5, 6, 7};
-  Test::map<vector,int,decltype(process)>(args, process);
-  vector<int> expected{1, 2, 5, 14, 42, 132, 429};
-
-  Test::assertEquals<int,vector,vector>(args, expected);
-
-  return EXIT_SUCCESS;
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
