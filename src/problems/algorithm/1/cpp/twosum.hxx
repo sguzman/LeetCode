018f9165fa32989a6f0b3693a3eb1bb2d8735b28
Salvador Guzman <guzmansalv@gmail.com>
2015-06-14 23:23:15 -0700

    twosum solution

--- src/problems/algorithm/1/cpp/twosum.hxx
+++ src/problems/algorithm/1/cpp/twosum.hxx
@@ -8,7 +8,22 @@ using std::unordered_map;
 
 class Solution {
 public:
-  vector<int> twoSum(vector<int>& nums, int target) {
+  vector<int> twoSum(const vector<int>& nums, const int target) {
+    unordered_map<int, int> values;
 
+    int delta;
+    for (int idx = 0; idx < nums.size(); ++idx) {
+      delta = target - nums[idx];
+
+      // If condition is true, then we're done here
+      if (values.find(delta) != values.cend()) {
+        return vector<int>{values[delta], idx + 1};
+      }
+
+      values[nums[idx]] = idx + 1;
+    }
+
+    // Should not happen but it's here to satisfy the compiler
+    return vector<int>{};
   }
 };
