ad2a798449f92874a161bed8465689f96eb4b5b2
Salvador Guzman <guzmansalv@gmail.com>
2015-06-14 23:50:56 -0700

    addtwonumbers gtest unit tests stub

---
+++ src/problems/algorithm/2/cpp/gtest.hxx
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <vector>
+#include <gtest/gtest.h>
+
+#include "../../../../include/cpp/gtest-array.hxx"
+#include "twosum.hxx"
+
+using std::vector;
+
+namespace {
+  class AddTwoNumbersTest : public testing::Test
+  {
+  protected:
+    AddTwoNumbersTest(void) {}
+    virtual ~AddTwoNumbersTest(void) {}
+    virtual void SetUp(void) {}
+    virtual void TearDown(void) {}
+  };
+
+  TEST(AddTwoNumbersTest, TwoSum1)
+  {
+    EXPECT_TRUE(true);
+  }
+}
