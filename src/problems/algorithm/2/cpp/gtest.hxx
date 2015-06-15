170a8066d7fea935d94f6a3e09d9e04b74d93d09
Salvador Guzman <guzmansalv@gmail.com>
2015-06-15 00:46:58 -0700

    working unit test

--- src/problems/algorithm/2/cpp/gtest.hxx
+++ src/problems/algorithm/2/cpp/gtest.hxx
@@ -3,7 +3,8 @@
 #include <vector>
 #include <gtest/gtest.h>
 
-#include "../../../../include/cpp/gtest-array.hxx"
+#include "../../../../include/cpp/LinkedList.hxx"
+#include "../../../../include/cpp/ListNode.hxx"
 #include "addtwonumbers.hxx"
 
 using std::vector;
@@ -20,6 +21,20 @@ namespace {
 
   TEST(AddTwoNumbersTest, TwoSum1)
   {
-    EXPECT_TRUE(true);
+    ListNode* input1 = new ListNode(2);
+    input1->next = new ListNode(4);
+    input1->next = new ListNode(3);
+
+    ListNode* input2 = new ListNode(5);
+    input2->next = new ListNode(6);
+    input2->next = new ListNode(4);
+
+    ListNode* actual = Solution{}.addTwoNumbers(input1, input2);
+
+    ListNode* expected = new ListNode(7);
+    expected->next = new ListNode(0);
+    expected->next = new ListNode(8);
+
+    EXPECT_TRUE(linkedMatch<ListNode>(expected, actual));
   }
 }
