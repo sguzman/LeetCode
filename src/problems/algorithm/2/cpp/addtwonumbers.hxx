6bd00b86eadd237cd0a839f1e770dbfa9642e6d0
Salvador Guzman <guzmansalv@gmail.com>
2015-06-15 01:42:17 -0700

    addtwonumbers solution

--- src/problems/algorithm/2/cpp/addtwonumbers.hxx
+++ src/problems/algorithm/2/cpp/addtwonumbers.hxx
@@ -13,7 +13,37 @@
 class Solution {
 public:
   ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-    ListNode* solution = new ListNode(0);
+    ListNode* solution = new ListNode{0};
+    ListNode* curr = solution;
+
+    ListNode* linkA = l1, * linkB = l2;
+
+    int sum;
+    bool isGreater10{false};
+    for (; linkA != nullptr && linkB != nullptr; (linkA = linkA->next), linkB = linkB->next) {
+      sum = linkA->val + linkB->val;
+
+      if (isGreater10) {
+        ++sum;
+      }
+
+      isGreater10 = sum > 9;
+
+      if (isGreater10) {
+        sum -= 10;
+      }
+
+      curr->val = sum;
+
+      if (linkA->next != nullptr) {
+        curr->next = new ListNode{1};
+        curr = curr->next;
+      }
+    }
+
+    if (isGreater10) {
+      curr->next = new ListNode{1};
+    }
 
     return solution;
   }
