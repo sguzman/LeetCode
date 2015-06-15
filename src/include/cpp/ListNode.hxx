cf5e5b7d856eb54b64720f177e4fb77d730552ae
Salvador Guzman <guzmansalv@gmail.com>
2015-06-14 23:51:50 -0700

    indent with two spaces

--- src/include/cpp/ListNode.hxx
+++ src/include/cpp/ListNode.hxx
@@ -2,7 +2,7 @@
 
 //Definition for singly-linked list.
 struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode(int x) : val(x), next(nullptr) {}
+  int val;
+  ListNode* next;
+  ListNode(int x) : val(x), next(nullptr) {}
 };
