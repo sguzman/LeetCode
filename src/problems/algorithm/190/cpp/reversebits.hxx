4c60de4a82c20923d0ff703637452a222afdddba
Salvador Guzman <guzmansalv@gmail.com>
2015-06-14 21:04:26 -0700

    add parentheses to expression

--- src/problems/algorithm/190/cpp/reversebits.hxx
+++ src/problems/algorithm/190/cpp/reversebits.hxx
@@ -7,7 +7,7 @@ public:
   uint32_t reverseBits(uint32_t n) {
     uint32_t final{};
     for (uint32_t i{}; n != 0; i++, n >>= 1) {
-      final |= (n & 1) << 31 - i;
+      final |= (n & 1) << (31 - i);
     }
 
     return final;
