fd40e91d07a20b051f2baa664f129762e0f00928
Salvador Guzman <guzmansalv@gmail.com>
2015-06-14 21:13:00 -0700

    get headers ready for solution

--- src/problems/algorithm/1/cpp/twosum.hxx
+++ src/problems/algorithm/1/cpp/twosum.hxx
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <vector>
+#include <unordered_map>
+
+using std::vector;
+using std::unordered_map;
+
 class Solution {
 public:
   vector<int> twoSum(vector<int>& nums, int target) {
