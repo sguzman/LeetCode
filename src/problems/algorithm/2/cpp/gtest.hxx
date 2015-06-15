42ec44ef81a65d65187ece89f2e1fc1a414fec1d
Salvador Guzman <guzmansalv@gmail.com>
2015-06-15 01:14:38 -0700

    clobbering element in linked list - fixed

--- src/problems/algorithm/2/cpp/gtest.hxx
+++ src/problems/algorithm/2/cpp/gtest.hxx
@@ -23,11 +23,11 @@ namespace {
   {
     ListNode* input1 = new ListNode(2);
     input1->next = new ListNode(4);
-    input1->next = new ListNode(3);
+    input1->next->next = new ListNode(3);
 
     ListNode* input2 = new ListNode(5);
     input2->next = new ListNode(6);
-    input2->next = new ListNode(4);
+    input2->next->next = new ListNode(4);
 
     ListNode* actual = Solution{}.addTwoNumbers(input1, input2);
 
