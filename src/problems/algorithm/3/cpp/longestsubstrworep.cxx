0993b0c98d03ab4e0e2d44da1f07d6026eb1c079
Salvador Guzman <guzmansalv@gmail.com>
2015-06-17 10:20:09 -0700

    finished gtest executable

--- src/problems/algorithm/3/cpp/longestsubstrworep.cxx
+++ src/problems/algorithm/3/cpp/longestsubstrworep.cxx
@@ -1,5 +1,12 @@
-//
-// Created by sguzman on 6/17/15.
-//
+#include <gtest/gtest.h>
 
 #include "longestsubstrworep.hxx"
+#include "gtest.hxx"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
