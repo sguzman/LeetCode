8d7b10a745feda6e7b9ad4ce8b38a19563b76ae9
Salvador Guzman <guzmansalv@gmail.com>
2015-06-18 21:03:17 -0700

    finished solution for longestsubstrworep

--- src/problems/algorithm/3/cpp/longestsubstrworep.hxx
+++ src/problems/algorithm/3/cpp/longestsubstrworep.hxx
@@ -7,11 +7,16 @@ using std::string;
 class Solution {
 public:
   int lengthOfLongestSubstring(const string& s) {
-    int length{}, index{};
-    int map[128] = {-1};
+    int length{}, index{}, map[128];
+
     std::fill_n(map, sizeof(map) /  sizeof(int), -1);
 
     for (int idx = 0; idx < s.size(); ++idx) {
+      index = std::max(map[s[idx]] + 1, index);
+
+      map[s[idx]] = idx;
+
+      length = std::max(length, idx - index + 1);
     }
 
     return length;
