4dc759ec2d8110a76513b85b634285f3c8ae9337
Salvador Guzman <salvador.guzman@logicmonitor.com>
2016-01-09 01:59:18 -0800

    finished two sum problem in java

---
+++ src/org/github/sguzman/leetcode/problems/twosum1/junit/SolutionTest.java
@@ -0,0 +1,84 @@
+package org.github.sguzman.leetcode.problems.twosum1.junit;
+
+import org.github.sguzman.leetcode.problems.twosum1.solution.Solution;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    Solution solution;
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+
+    private void assertSingleTest(int[] nums, int k, int[] expected) {
+        assertEquals("Expected solution not size 2", expected.length, 2);
+
+        final int[] actual = solution.twoSum(nums, k);
+
+        assertArrayEquals("Actual " + Arrays.toString(actual) + " does not match expected " + Arrays.toString(expected),
+                expected, actual);
+    }
+
+    @Test
+    public void testTwoSum1() throws Exception {
+        final int[] nums = {2, 7, 11, 15};
+        final int k = 9;
+
+        final int[] expected = {1, 2};
+
+        assertSingleTest(nums, k, expected);
+    }
+
+    @Test
+    public void testTwoSum2() throws Exception {
+        final int[] nums = {3, 7, 11, 15, 6, 2, 9};
+        final int k = 8;
+
+        final int[] expected = {5, 6};
+
+        assertSingleTest(nums, k, expected);
+    }
+
+    @Test
+    public void testTwoSum3() throws Exception {
+        final int[] nums = {3, 7, 11, 15, 6, 2, 9};
+        final int k = 24;
+
+        final int[] expected = {4, 7};
+
+        assertSingleTest(nums, k, expected);
+    }
+
+    @Test
+    public void testTwoSum4() throws Exception {
+        final int[] nums = {3, 2, 4};
+        final int k = 6;
+
+        final int[] expected = {2, 3};
+
+        assertSingleTest(nums, k, expected);
+    }
+
+    @Test
+    public void testTwoSum5() throws Exception {
+        final int[] nums = {0, 4, 3, 0};
+        final int k = 0;
+
+        final int[] expected = {1, 4};
+
+        assertSingleTest(nums, k, expected);
+    }
+}
---
+++ src/org/github/sguzman/leetcode/problems/twosum1/solution/Solution.java
@@ -0,0 +1,37 @@
+package org.github.sguzman.leetcode.problems.twosum1.solution;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1. Two Sum My Submissions Question
+ Total Accepted: 172506 Total Submissions: 852698 Difficulty: Medium
+ Given an array of integers, find two numbers such that they add up to a specific target number.
+
+ The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+ You may assume that each input would have exactly one solution.
+
+ Input: numbers={2, 7, 11, 15}, target=9
+ Output: index1=1, index2=2
+ */
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        // Contains the final indices
+        int[] idxs = new int[2];
+
+        for (int idx = 0; idx < nums.length; ++idx) {
+            if (!map.containsKey(nums[idx])) {
+                map.put(target - nums[idx], idx);
+            } else {
+                idxs[0] = map.get(nums[idx]) + 1;
+                idxs[1] = idx + 1;
+
+                break;
+            }
+        }
+
+        return idxs;
+    }
+}
