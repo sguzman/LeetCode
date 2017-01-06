62056062fb421479ddcd91a867d89f1c5b4a1707
Salvador Guzman <guzmansalv@gmail.com>
2017-01-06 01:24:24 -0800

    added gulp-{util,flowtype,sourcemaps} deps

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,9 +2,12 @@
 
 import gulp from 'gulp';
 import babel from 'gulp-babel'
+import util from 'gulp-util'
+import mocha from 'gulp-mocha'
 
 gulp.task('default', () => {
     return gulp.src('./src/js/leet.js')
         .pipe(babel())
-        .pipe(gulp.dest('dist'));
+        .pipe(mocha({reporter: 'spec'}))
+        .on('error', util.log);
 });
